dd06fc3c1b8f91ef4451201d6d9091800d39a27f
RendyDharmaPutra <rendydharmaputra424@gmail.com>
2024-06-16 14:54:31 +0700

    add header and footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+	return (
+		<div className="p-6 flex justify-center w-full bg-white">
+			<p className="font-semibold">
+				©2024 • Search Movies <span className="font-normal"> by </span>{" "}
+				<a href="" className="font-bold text-blue-500">
+					Archilst
+				</a>
+			</p>
+		</div>
+	);
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+	return (
+		<div className="py-6 px-10 w-full bg-white">
+			<h1 className="font-bold text-xl text-blue-600">Search Movie</h1>
+		</div>
+	);
+}
