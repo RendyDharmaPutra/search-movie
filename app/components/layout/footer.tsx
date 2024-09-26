2d445a63c2093d8e8db269fb43134bfa19e07c71
Rendy DharmaPutra <72882576+RendyDharmaPutra@users.noreply.github.com>
2024-09-26 10:26:12 +0700

    Update footer.tsx

--- app/components/layout/footer.tsx
+++ app/components/layout/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer(): JSX.Element {
 	return (
 		<div className="p-4 flex justify-center w-full bg-white">
 			<p className="text-sm font-semibold text-gray-800">
-				©2024 • Simple Book App{" "}
+				©2024 • Search Movie{" "}
 				<span className="font-normal text-gray-800">by</span>{" "}
 				<Link
 					target="_blank"
